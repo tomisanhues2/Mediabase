0e7408e93f57cddc243447d817d66e6514126b93
Tomas Sanhueza <tomisanhues2@gmail.com>
2018-08-03 13:36:40 -0400

    Many changes

--- src/resources/IMovie.java
+++ src/resources/IMovie.java
@@ -1,11 +1,11 @@
 package resources;
 
-import java.util.ResourceBundle;
 
 public interface IMovie {
 
 
-    String DATABASE_API_KEY = "&plot=full&apikey=9165ddc0";
-    String DATABASE_URL_STRING = "http://www.omdbapi.com/?";
+    String DATABASE_API_KEY = "?api_key=9ea34de517a4414366db6520cafda608";
+    String DATABASE_URL_STRING = "https://api.themoviedb.org/3/";
+    String DATABASE_IMAGE_URL_STRING = "https://image.tmdb.org/t/p/";
 
 }
